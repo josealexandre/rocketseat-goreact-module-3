423e3a85af94a40c45f4a3ca9e33c2b74193aed3
Alexandre Junior <jasj_jr@hotmail.com>
2019-07-20 17:07:44 -0300

    Removing actions and reducers built for tests purposes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,11 @@ import "./config/ReactotronConfig";
 import { Provider } from "react-redux";
 import store from "./store/index";
 
-import TodoList from "./TodoList";
-
 console.tron.log({ Hello: "world" });
 
 const App = () => (
     <Provider store={store}>
-        <TodoList>
-            <h1>Hello World</h1>
-        </TodoList>
+        <h1>Hello World</h1>
     </Provider>
 );
 
