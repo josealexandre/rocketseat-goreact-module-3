717b9f231bd56ed86bfe1774a60897e119bb38fa
Alexandre Junior <jasj_jr@hotmail.com>
2019-07-21 12:37:11 -0300

    Creating footer

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Main from "../pages/main";
+import Footer from "../components/Footer";
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route path="/" component={Main} />
         </Switch>
+
+        <Footer />
     </BrowserRouter>
 );
 
